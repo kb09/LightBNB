ccb2ce1219f8778780378177387f53933d5f3985
kartik <kabakshi@hotmail.ca>
2021-11-24 20:35:17 -0500

    add data to users

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,27 @@
+-- Users
+
+-- Use $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u. as every users password. 
+-- It's the word password hashed with bcrypt.
+
+INSERT INTO users (name, email, password)
+VALUES 
+( 'Eva Stanley',
+'sebastianguerra@ymail.com',
+'$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+),
+( 'Pam Beesly',
+'pamspam@ymail.com',
+'$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+),
+( 'Dwight Schrute',
+'Beetsarethebesta@ymail.com',
+'$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+),
+( 'Santa Claus',
+'santaslist@ymail.com',
+'$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+),
+( 'Leroy Hart  ',
+'jaycereynolds@inbox.com',
+'$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
+)
