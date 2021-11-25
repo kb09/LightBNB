d9d92a7e49206abff0af7d03fc2cc53d2044b5ba
kartik <kabakshi@hotmail.ca>
2021-11-24 21:37:13 -0500

    adding reservations data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -24,86 +24,118 @@ VALUES
 ( 'Leroy Hart  ',
 'jaycereynolds@inbox.com',
 '2a10FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'
-)
+);
 
 -- properties
 
 -- Don't worry about adding long descriptions. Just use the word "description"
 
-INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces,
-    number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active
-    );
+INSERT INTO properties (
+  owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces,
+  number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active
+  );
 
-    VALUES 
-    (
-      1,
-      'Speed lamp',
-      'description',
-      'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350',
-      'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',
-      93061,
-      6,
-      4,
-      8,
-      'Canada',
-      '536 Namsub Highway',
-      'Sotboske',
-      'Quebec',
-      '28142',
-      true
+ VALUES 
+ (
+   1,
+   'Speed lamp',
+   'description',
+   'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350',
+   'https://images.qpexels.com/photos/2086676/pexels-photo-2086676.jpeg',
+   93061,
+   6,
+   4,
+   8,
+   'Canada',
+   '536 Namsub Highway',
+   'Sotboske',
+   'Quebec',
+   '28142',
+   true
   ),
   
   (
-      2,
-      'Habit mix ',
-      'description',
-      'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350',
-      'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg',
-      46052,
-      0,
-      5,
-      6,
-      'Canada',
-      '1650 Hejto Center ',
-      'Genwezuj',
-      'Newfoundland And Labrador ',
-      '44583',
-      true
+    2,
+    'Habit mix ',
+    'description',
+    'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350',
+    'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg',
+    46052,
+    0,
+    5,
+    6,
+    'Canada',
+    '1650 Hejto Center ',
+    'Genwezuj',
+    'Newfoundland And Labrador ',
+    '44583',
+    true
   ),
 
   (
-      3,
-      'Headed know',
-      'description',
-      'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&h=350 ',
-      'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg',
-      82640,
-      0,
-      5,
-      5,
-      'Canada',
-      '513 Powov Grove',
-      'Jaebvap',
-      'Ontario',
-      '38051  ',
-      true
+    3,
+    'Headed know',
+    'description',
+    'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&h=350 ',
+    'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg',
+    82640,
+    0,
+    5,
+    5,
+    'Canada',
+    '513 Powov Grove',
+    'Jaebvap',
+    'Ontario',
+    '38051  ',
+    true
   ),
 
   (
-      4,
-      'Fun glad ',
-      'description',
-      ' https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&h=350',
-      'https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg',
-      34291,
-      6,
-      6,
-      4,
-      'Canada',
-      '169 Nuwug Circle',
-      'Vutgapha',
-      'Newfoundland And Labrador',
-      ' 00159',
-      true
+    4,
+    'Fun glad ',
+    'description',
+    ' https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&h=350',
+    'https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg',
+    34291,
+    6,
+    6,
+    4,
+    'Canada',
+    '169 Nuwug Circle',
+    'Vutgapha',
+    'Newfoundland And Labrador',
+    ' 00159',
+    true
   );  
     
+  -- reservations 
+
+  INSERT INTO reservations (start_date, end_date, property_id, guest_id) 
+
+  VALUES (
+  '2018-09-11',   
+  '2018-09-26',             
+  '2',    
+  '3'
+  ),
+
+  (
+  '2019-01-04',  
+  '2019-02-01',        
+  '2',      
+  '2',
+  ),
+
+  (
+  '2021-10-01',  
+  '2021-10-14',            
+  '1',     
+  '4'
+  ),
+
+  (
+  '2014-10-21',
+  '2014-10-21',
+  '3', 
+  '5'
+  );
